54b4e3bed8c3b05fbb167b97faa762daa46c7888
ExypnosDevloper <grubnoreplygooogle@gmail.com>
2018-10-07 15:17:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ restService.post("/audio", function(req, res) {
     //Speech Synthesis Markup Language 
     case "suicide":
       speech =
-        '<speak><audio src="https://firebasestorage.googleapis.com/v0/b/peace-ca02b.appspot.com/o/test.mp3?alt=media&token=3bb5992a-d5d3-4f57-b3b4-24adff7f63fb">did not get your audio file</audio></speak>';
+        '<speak><audio src="https://firebasestorage.googleapis.com/v0/b/peace-ca02b.appspot.com/o/test.mp3?alt=media&token=3bb5992a-d5d3-4f57-b3b4-24adff7f63fb"></audio>did not get your audio file</speak>';
       break;
     case "end":
       speech =
