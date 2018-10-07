f25e070115bb306a5524a94435ec9dcc2fb325b5
ExypnosDevloper <grubnoreplygooogle@gmail.com>
2018-10-07 18:12:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const{ dialogflow }=require('actions-on-google');
+const app = dialogflow({
+	debug: true
+})
 
 const restService = express();
 
@@ -14,28 +18,18 @@ restService.use(
 restService.use(bodyParser.json());
 
 restService.post("/audio", function(req, res) {
+	if (app.intent("help")==help) {
+		app.intent("help", conv=>{
+			return conv.ask('You can count on following:\n\n1.Aasra Address:104, Sunrise Arcade, Plot No. 100, Sector 16, Koparkhairane, Navi Mumbai - 40070924 Hour Helpline 022-27546669 Office:(10am to 7pm) 022-27546667 Email: aasrahelpline@yahoo.com Contact Person: Johnson Thomas, 9820466726')
+		});
+
+	}
   var speech = "";
-  switch (req.body.result.parameters.suicide.toLowerCase()) {
+  switch (req.body.result.parameters.AudioSample.toLowerCase()) {
     //Speech Synthesis Markup Language 
-    case "suicide":
+    case "music one":
       speech =
-        '<speak><audio src="https://bit.ly/2NsXDlS"></audio>Put on your earphones.. Close your eyes and just Focus.</speak>';
-      break;
-    case "end":
-      speech =
-        '<speak><audio src="https://bit.ly/2NsXDlS"></audio>Put on your earphones.. Close your eyes and just Focus.</speak>';
-      break;
-    case "destruct":
-      speech =
-        '<speak><audio src="https://bit.ly/2NsXDlS"></audio>Put on your earphones.. Close your eyes and just Focus.</speak>';
-      break;
-    case "finish":
-      speech =
-        '<speak><audio src="https://bit.ly/2NsXDlS"></audio>Put on your earphones.. Close your eyes and just Focus.</speak>';
-      break;
-    case "kill":
-      speech =
-        '<speak><audio src="https://bit.ly/2NsXDlS"></audio>Put on your earphones.. Close your eyes and just Focus.</speak>>';
+        '<speak><audio src="https://firebasestorage.googleapis.com/v0/b/peace-ca02b.appspot.com/o/test.mp3?alt=media&token=3bb5992a-d5d3-4f57-b3b4-24adff7f63fb">did not get your audio file</audio></speak>';
       break;
   }
   return res.json({
