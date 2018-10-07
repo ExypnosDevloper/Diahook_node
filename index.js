0c4b2cad61ff9689cb25dc111d7afefba1194d94
ExypnosDevloper <grubnoreplygooogle@gmail.com>
2018-10-07 12:41:58 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+"use strict";
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const restService = express();
+
+restService.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+
+restService.use(bodyParser.json());
+
+restService.post("/audio", function(req, res) {
+  var speech = "";
+  switch (req.body.result.parameters.AudioSample.toLowerCase()) {
+    //Speech Synthesis Markup Language 
+    case "music one":
+      speech =
+        '<speak><audio src="https://firebasestorage.googleapis.com/v0/b/peace-ca02b.appspot.com/o/test.mp3?alt=media&token=3bb5992a-d5d3-4f57-b3b4-24adff7f63fb">did not get your audio file</audio></speak>';
+      break;
+  }
+  return res.json({
+    speech: speech,
+    displayText: speech,
+    source: "Team .Onion"
+  });
+});
+
+restService.listen(process.env.PORT || 8000, function() {
+  console.log("Server up and listening");
+});
