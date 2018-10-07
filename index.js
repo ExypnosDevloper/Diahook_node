7ffb5bf65c41e88d36b3005d1b1d0f2981e6e85f
ExypnosDevloper <grubnoreplygooogle@gmail.com>
2018-10-07 13:26:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,35 +13,17 @@ restService.use(
 
 restService.use(bodyParser.json());
 
-restService.post("/audio", function(req, res) {
-  var speech = "";
-  switch (req.body.result.parameters.heal.toLowerCase()) {
-    //Speech Synthesis Markup Language 
-    case "suicide":
-      speech =
-        '<speak><audio src="https://firebasestorage.googleapis.com/v0/b/peace-ca02b.appspot.com/o/test.mp3?alt=media&token=3bb5992a-d5d3-4f57-b3b4-24adff7f63fb">did not get your audio file</audio></speak>';
-      break;
-    case "end":
-      speech =
-        '<speak><audio src="https://firebasestorage.googleapis.com/v0/b/peace-ca02b.appspot.com/o/test.mp3?alt=media&token=3bb5992a-d5d3-4f57-b3b4-24adff7f63fb">did not get your audio file</audio></speak>';
-      break;
-    case "destruct":
-      speech =
-        '<speak><audio src="https://firebasestorage.googleapis.com/v0/b/peace-ca02b.appspot.com/o/test.mp3?alt=media&token=3bb5992a-d5d3-4f57-b3b4-24adff7f63fb">did not get your audio file</audio></speak>';
-      break;
-    case "finish":
-      speech =
-        '<speak><audio src="https://firebasestorage.googleapis.com/v0/b/peace-ca02b.appspot.com/o/test.mp3?alt=media&token=3bb5992a-d5d3-4f57-b3b4-24adff7f63fb">did not get your audio file</audio></speak>';
-      break;
-    case "kill":
-      speech =
-        '<speak><audio src="https://firebasestorage.googleapis.com/v0/b/peace-ca02b.appspot.com/o/test.mp3?alt=media&token=3bb5992a-d5d3-4f57-b3b4-24adff7f63fb">did not get your audio file</audio></speak>';
-      break;
-  }
+restService.post("/echo", function(req, res) {
+  var speech =
+    req.body.result &&
+    req.body.result.parameters &&
+    req.body.result.parameters.echoText
+      ? req.body.result.parameters.echoText
+      : "Seems like some problem. Speak again.";
   return res.json({
     speech: speech,
     displayText: speech,
-    source: "Team .Onion"
+    source: "webhook-echo-sample"
   });
 });
 
