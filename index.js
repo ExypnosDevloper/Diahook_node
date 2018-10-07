f95daf060f6fd864fe8805d81714a292a6a1cf80
ExypnosDevloper <grubnoreplygooogle@gmail.com>
2018-10-07 19:46:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,8 +18,19 @@ restService.post("/audio", function(req, res) {
   switch (req.body.result.parameters.suicide.toLowerCase()) {
     //Speech Synthesis Markup Language 
     case "suicide":
-      speech =
-        '<speak><audio src="https://bit.ly/2NsXDlS"></audio>Put on your earphones.. Close your eyes and just Focus.</speak>';
+       if (!conv.surface.capabilities.has('actions.capability.MEDIA_RESPONSE_AUDIO')) {
+  conv.ask('Sorry, this device does not support audio playback.');
+  return;
+}
+conv.ask(new MediaObject({
+  name: 'Jazz in Paris',
+  url: 'http://storage.googleapis.com/automotive-media/Jazz_In_Paris.mp3',
+  description: 'A funky Jazz tune',
+  icon: new Image({
+    url: 'http://storage.googleapis.com/automotive-media/album_art.jpg',
+    alt: 'Media icon',
+  }),
+}));
       break;
     case "end":
       speech =
