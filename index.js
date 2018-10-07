5152fe525b323b9d7a87c17719b51089d434e584
ExypnosDevloper <grubnoreplygooogle@gmail.com>
2018-10-07 19:18:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
-
 const restService = express();
 
 restService.use(
@@ -15,13 +14,28 @@ restService.use(
 restService.use(bodyParser.json());
 
 restService.post("/audio", function(req, res) {
-
   var speech = "";
-  switch (req.body.result.parameters.AudioSample.toLowerCase()) {
+  switch (req.body.result.parameters.suicide.toLowerCase()) {
     //Speech Synthesis Markup Language 
-    case "music one":
+    case "suicide":
       speech =
-        '<speak><audio src="https://firebasestorage.googleapis.com/v0/b/peace-ca02b.appspot.com/o/test.mp3?alt=media&token=3bb5992a-d5d3-4f57-b3b4-24adff7f63fb">did not get your audio file</audio></speak>';
+        '<speak><audio src="https://bit.ly/2NsXDlS"></audio>Put on your earphones.. Close your eyes and just Focus.</speak>';
+      break;
+    case "end":
+      speech =
+        '<speak><audio src="https://bit.ly/2NsXDlS"></audio>Put on your earphones.. Close your eyes and just Focus.</speak>';
+      break;
+    case "destruct":
+      speech =
+        '<speak><audio src="https://bit.ly/2NsXDlS"></audio>Put on your earphones.. Close your eyes and just Focus.</speak>';
+      break;
+    case "finish":
+      speech =
+        '<speak><audio src="https://bit.ly/2NsXDlS"></audio>Put on your earphones.. Close your eyes and just Focus.</speak>';
+      break;
+    case "kill":
+      speech =
+        '<speak><audio src="https://bit.ly/2NsXDlS"></audio>Put on your earphones.. Close your eyes and just Focus.</speak>>';
       break;
   }
   return res.json({
